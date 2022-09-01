a4b3748eb3f338135ee34a2095062420d484cbbe
Artem Yunker <art-yunker@yandex.ru>
2022-09-01 19:56:26 +0200

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+string input1 = Console.ReadLine();
+int number1 = int.Parse(input1);
+ 
+Console.WriteLine("Введите второе число");
+string input2 = Console.ReadLine();
+int number2 = int.Parse(input2);
+
+if (number1 > number2)
+{
+Console.WriteLine($"max = {number1}");
+}
+else
+{
+    Console.WriteLine($"max = {number2}");
+}
