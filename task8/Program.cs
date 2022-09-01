1f90d9281bd410ceebf8fa125004df0c6293cf07
Artem Yunker <art-yunker@yandex.ru>
2022-09-01 20:08:42 +0200

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число"); 
+string input = Console.ReadLine();
+int number = int.Parse(input);
+Console.WriteLine("Четные числа:"); 
+for (int index = 2; index <= number; index = index + 2)
+{
+    Console.WriteLine(index);
+}
+
