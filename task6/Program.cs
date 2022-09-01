803fbb989c45e79f9bcef7ec533393279e56dc9f
Artem Yunker <art-yunker@yandex.ru>
2022-09-01 20:05:50 +0200

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+if (number%2 == 0)
+{
+Console.WriteLine($"Число {number} - четное");
+}
+else 
+{
+    Console.WriteLine($"Число {number} - не четное");
+}
